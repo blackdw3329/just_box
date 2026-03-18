964eebd07237a9736651576f4c70160fe4e29248
김동욱 <127175095+blackdw3329@users.noreply.github.com>
2026-03-18 10:52:34 +0900

    Update test1.cpp

--- cpp/test1.cpp
+++ cpp/test1.cpp
@@ -1,25 +1,34 @@
+// 실습과제 2
+
 #include <iostream>
 using namespace std;
 
-class Triangle
-{
+class Triangle {
 public:
-	int height;
 	int width;
+	int height;
+	Triangle();
+	Triangle(int w, int h);
 	double getArea();
+
 };
-double Triangle::getArea()
-{
-	return 0.5 * width * height;
+
+Triangle::Triangle(int w, int h) {
+	width = w;
+	height = h;
 }
-int main()
-{
-	Triangle tri;
-	tri.width = 3;
-	tri.height = 5;
-	cout << "삼각형의 면적은 " << tri.getArea() << endl;
-	cout << sizeof(tri.getArea()) << endl;
 
+Triangle::Triangle() :Triangle(1, 1) { }
 
+double Triangle::getArea() {
+	return width * height * 1.0 / 2.0;
+}
+
+int main() {
+	Triangle tri1; // 밑변,높이 1로 초기화
+	cout << "삼각형의 면적은 " << tri1.getArea() << endl;
+
+	Triangle tri2(2, 4); // 밑변=2,높이=4로 초기화
+	cout << "삼각형의 면적은 " << tri2.getArea() << endl;
 	return 0;
 }
