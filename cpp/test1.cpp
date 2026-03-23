8149f2172508cb8d65a7810fab9c679baa2c91fb
김동욱 <127175095+blackdw3329@users.noreply.github.com>
2026-03-23 10:20:50 +0900

    Update test1.cpp

--- cpp/test1.cpp
+++ cpp/test1.cpp
@@ -87,3 +87,45 @@ int main() {
 	cout << "삼각형의 면적은" << tri1.getArea() << endl;
 	return 0;
 }
+
+//실습문제 4
+#include <iostream>
+
+using namespace std;
+
+class Sphere {
+public:
+	int radius;
+	Sphere();
+	Sphere(int r);
+	~Sphere();
+	double getVolume();
+
+};
+Sphere::Sphere() {
+	radius = 1;
+	cout << "반지름 " << radius << "인 원 생성" << endl;
+}
+
+Sphere::Sphere(int r) {
+	radius = r;
+	cout << "반지름 " << radius << "인 원 생성" << endl;
+}
+
+Sphere::~Sphere() {
+	cout << "반지름 " << radius << "인 원 소멸" << endl;
+}
+
+double Sphere::getVolume() {
+	return radius * radius * radius * 3.14 * 4 / 3;
+}
+
+Sphere sph1;
+Sphere sph2(3);
+
+// 클래스 정의
+int main() {
+	cout << "삼각형의 면적은" << sph2.getVolume() << endl;
+	cout << "삼각형의 면적은" << sph1.getVolume() << endl;
+	return 0;
+}
