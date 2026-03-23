db855679575bce40ce8ec1cb468a67fd87ef50bd
김동욱 <127175095+blackdw3329@users.noreply.github.com>
2026-03-23 10:51:48 +0900

    Update test1.cpp

--- cpp/test1.cpp
+++ cpp/test1.cpp
@@ -118,12 +118,55 @@ double Sphere::getVolume() {
 	return radius * radius * radius * 3.14 * 4 / 3;
 }
 
-Sphere sph1;
-Sphere sph2(3);
 
 // 클래스 정의
 int main() {
-	cout << "삼각형의 면적은" << sph2.getVolume() << endl;
-	cout << "삼각형의 면적은" << sph1.getVolume() << endl;
+	Sphere sph1;
+	cout << "구의 부피는 " << sph1.getVolume() << endl;
+	Sphere sph2(3);
+	cout << "구의 부피는 " << sph2.getVolume() << endl;
+	return 0;
+}
+
+// 실습문제5
+#include <iostream>
+
+using namespace std;
+
+class Sphere {
+public:
+	int radius;
+	Sphere();
+	Sphere(int r);
+	~Sphere();
+	double getVolume();
+
+};
+Sphere::Sphere() {
+	radius = 1;
+	cout << "반지름 " << radius << "인 원 생성" << endl;
+}
+
+Sphere::Sphere(int r) {
+	radius = r;
+	cout << "반지름 " << radius << "인 원 생성" << endl;
+}
+
+Sphere::~Sphere() {
+	cout << "반지름 " << radius << "인 원 소멸" << endl;
+}
+
+double Sphere::getVolume() {
+	return radius * radius * radius * 3.14 * 4 / 3;
+}
+
+Sphere sph1(10);
+Sphere sph2(20);
+
+// 클래스 정의
+int main() {
+
+	cout << "구의 부피는 " << sph1.getVolume() << endl;
+	cout << "구의 부피는 " << sph2.getVolume() << endl;
 	return 0;
 }
