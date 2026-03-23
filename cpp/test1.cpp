700db79305579cf634d8fdc8b6e52af99f0e5550
김동욱 <127175095+blackdw3329@users.noreply.github.com>
2026-03-23 10:03:20 +0900

    Update test1.cpp

--- cpp/test1.cpp
+++ cpp/test1.cpp
@@ -1,6 +1,7 @@
 // 실습과제 2
 
 #include <iostream>
+
 using namespace std;
 
 class Triangle {
@@ -9,26 +10,35 @@ public:
 	int height;
 	Triangle();
 	Triangle(int w, int h);
+	~Triangle();
 	double getArea();
 
 };
+Triangle::Triangle() {
+	width = 1; height = 1;
+	cout << "밑변 " << width << "높이" << height << "인 삼각형 생성" << endl;
+}
 
 Triangle::Triangle(int w, int h) {
-	width = w;
-	height = h;
+	width = w; height = h;
+	cout << "밑변 " << width << "높이" << height << "인 삼각형 생성" << endl;
 }
 
-Triangle::Triangle() :Triangle(1, 1) { }
+Triangle::~Triangle() {
+	cout << "밑변 " << width << "높이" << height << "인 삼각형 소멸" << endl;
+}
 
 double Triangle::getArea() {
-	return width * height * 1.0 / 2.0;
+	return 0.5 * width * height;
 }
 
-int main() {
-	Triangle tri1; // 밑변,높이 1로 초기화
-	cout << "삼각형의 면적은 " << tri1.getArea() << endl;
 
-	Triangle tri2(2, 4); // 밑변=2,높이=4로 초기화
-	cout << "삼각형의 면적은 " << tri2.getArea() << endl;
+// 클래스 정의
+int main() {
+	Triangle tri1; //밑변,높이 1로 초기화
+	cout << "삼각형의 면적은" << tri1.getArea() << endl;
+	Triangle tri2(2, 4); //밑변=2,높이=4로 초기화
+	cout << "삼각형의 면적은" << tri2.getArea() << endl;
 	return 0;
 }
+
