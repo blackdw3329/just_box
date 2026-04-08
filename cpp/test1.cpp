bc6040a410a0a0aad47851a15388cd7cf6909c5b
김동욱 <127175095+blackdw3329@users.noreply.github.com>
2026-04-08 10:36:41 +0900

    Update test1.cpp

--- cpp/test1.cpp
+++ cpp/test1.cpp
@@ -1,74 +1,140 @@
 // 실습2
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
-	int  n;
-	int total = 0;
-	double avg;
-	cout << "입력할 정수의 개수를 입력하시오:";
-	cin >> n;
-	int* p = new int[n];
-	cout << n << "개의 정수를 입력 하시오." << endl;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> p[i];
-	}
-	for (int j = 0; j < n; j++)
-	{
-		total += p[j];
-	}
-	avg = total / n;
-	cout << "평균값은 " << avg << "입니다" << endl;
-	delete[]p;
+class Circle {
+	int radius;
+public:
+	Circle();
+	Circle(int r);
+	~Circle();
+	void setRadius(int r) { radius = r; }
+	double getArea() { return 3.14 * radius * radius; }
+};
+Circle::Circle() {
+	radius = 1; cout << "생성자 실행 radius = " << radius << endl;
 }
-
-//실습2
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	int  n;
-	int max;
-	cout << "입력할 정수의 개수를 입력하시오:";
-	cin >> n;
-	int* p = new int[n];
-	cout << n << "개의 정수를 입력 하시오." << endl;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> p[i];
-	}
-	max = p[0];
-	for (int j = 0; j < n; j++)
-	{
-		if (max <= p[j]) max = p[j];
-	}
-	cout << "최대값은 " << max << "입니다" << endl;
-	delete[]p;
+Circle::Circle(int r) {
+	radius = r; cout << "생성자 실행 radius = " << radius << endl;
 }
-
+Circle::~Circle() {
+	cout << "소멸자 실행 radius = " << radius << endl;
+}
+int main() {
+	int radius;
+	while (true) {
+		cout << "반지름 입력(음수이면 종료)>> ";
+		cin >> radius;
+		if (radius < 0) break;
+		Circle* p = new Circle(radius);
+		cout << "원의 면적:" << p->getArea() << endl;
+		 //delete p;  // 메모리 해제를 안해주면 메모리 누수 발생
+	}
+	return 0;
+}
 //실습3
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
-	int  n;
-	int max;
-	cout << "저장할 문자열의 크기를 입력하세요:";
-	cin >> n;
-	char* p = new char[n+1];
-	cout << "문자열을 입력하시오: ";
-	cin.ignore();
-	cin.getline(p, n + 1);
-	cout << "입력한 문자열은: " << p << "입니다." << endl;
-
-	delete[]p;
+class Circle {
+	int radius;
+public:
+	Circle();
+	Circle(int r);
+	~Circle();
+	void setRadius(int r) { radius = r; }
+	double getArea() { return 3.14 * radius * radius; }
+};
+Circle::Circle() {
+	radius = 1; cout << "생성자 실행 radius = " << radius << endl;
 }
+Circle::Circle(int r) {
+	radius = r; cout << "생성자 실행 radius = " << radius << endl;
+}
+Circle::~Circle() {
+	cout << "소멸자 실행 radius = " << radius << endl;
+}
+
+int main() {
+	Circle* pArray = new Circle[3];
+	for (int i = 0; i < 3; i++) {
+		cout << pArray->getArea() << '\n';
+		pArray++; // 해제할때 pArray의 주소가  달라져있어 문제가된다
+	}
+	delete[] pArray;
+	return 0;
+}
+
+// 해결한 main 함수
+/*
+int main() {
+	Circle* pArray = new Circle[3];
+	Circle* p = pArray;
+	for (int i = 0; i < 3; i++) {
+		cout << p->getArea() << '\n';
+		p++; // 다른 포인터를 만들어 주소를 따로 접근한다
+	}
+	delete[] pArray;
+	return 0;
+}
+*/
+//실습4
+#include <iostream>
+using namespace std;
+class Triangle {
+	int width,height;
+public:
+	Triangle();
+	Triangle(int w, int h);
+	~Triangle();
+	double getArea() { return (0.5 * width * height); }
+};
+Triangle::Triangle():Triangle(1,1) { }
+Triangle::Triangle(int w, int h) {
+	width = w; height = h;
+	cout << "밑변" << width << " 높이" << height << "인 삼각형 생성" << endl;
+}
+Triangle::~Triangle() {
+	cout << "밑변" << width << " 높이" << height << "인 삼각형 소멸" << endl;
+}
+int main() {
+	Triangle* pArray = new Triangle[3]{ Triangle(),Triangle(2,2),Triangle(4,4) };
+	
+	Triangle* p = pArray;
+	for (int i = 0; i < 3; i++) {
+		cout << "삼각형의 면적은 " << p[i].getArea() << '\n';
+		p++;
+	}
+	delete[] pArray;
+	return 0;
+}
+
+//실습5
+#include <iostream>
+using namespace std;
+class Sphere {
+	int radius;
+public:
+	Sphere();
+	~Sphere() {}
+	void setRadius(int r) { cout << r << "반지름 변경" << endl; radius = r; }
+	double getArea() { cout << "부피 출력" << endl; return 3.14 * radius * radius * radius * 4 / 3; }
+};
+Sphere::Sphere() {
+	radius = 1;
+}
+int main() {
+	cout << "생성하고자 하는 구의 개수: ";
+	int n, radius;
+	cin >> n;
+	Sphere* pArray = new Sphere[n];
+	for (int i = 0; i < n; i++) {
+		cout << "구" << i + 1 << "의 반지름: ";
+		cin >> radius;
+		(pArray + i)->setRadius(radius);
+	}
+	for (int j = 0; j < n; j++) {
+		cout << "구" << j + 1 << "의 부피: " << (pArray + j)->getArea() << endl;
+	}
+	delete[] pArray;
+	return 0;
+}
+
