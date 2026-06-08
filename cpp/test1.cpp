bc2e4efa37943864c4be75efa18b91d7dabf406a
김동욱 <127175095+blackdw3329@users.noreply.github.com>
2026-06-08 10:46:10 +0900

    Update test1.cpp

--- cpp/test1.cpp
+++ cpp/test1.cpp
@@ -1,23 +1,94 @@
 #include <iostream>
+
 using namespace std;
 
-class Rectangle {
-	int width, height;
+class Complex {
+	int real, img;
 public:
-	Rectangle() :Rectangle(1, 1) {}
-	Rectangle(int w) { this->width = w; this->height = 1; }
-	Rectangle(int w, int h) { this->width = w; this->height = h; }
+	Complex(int r = 0, int i = 0) { this->real = r; this->img = i; cout << "복소수 " << real << '+' << img << "j 생성" << endl;
+	}
+	Complex& operator++();
+	Complex operator--(int x);
 	void show() {
-		cout << "사각형 폭은 " << width << " 높이는 " << height << endl;
+		if(img<0) cout << real << img << 'j' << endl;
+		else cout << real << '+' << img << 'j' << endl;
 	}
 };
 
+Complex& Complex::operator++(){
+	real++;
+	img++;
+	return *this;
+}
+
+Complex Complex::operator--(int x){
+	Complex tmp = *this;
+	real--;
+	img--;
+	return tmp;
+}
+
+
+
+
 int main() {
-	Rectangle rect0;
-	rect0.show();
-	Rectangle rect1(10);
-	rect1.show();
-	Rectangle rect2(10, 20);
-	rect2.show();
+	Complex x(2, -3);
+	++x;
+	cout << "증가결과 ";
+	x.show();
+	x--;
+	cout << "감소결과 ";
+	x.show();
 	return 0;
 }
+
+
+
+
+# 실습과제 2
+#include <iostream>
+
+using namespace std;
+
+class Complex {
+	int real, img;
+public:
+	Complex(int r = 0, int i = 0) {
+		this->real = r; this->img = i; cout << "복소수 " << real << '+' << img << "j 생성" << endl;
+	}
+	friend Complex& operator++(Complex& op);
+	friend Complex operator--(Complex op, int x);
+	void show() {
+		if (img < 0) cout << real << img << 'j' << endl;
+		else cout << real << '+' << img << 'j' << endl;
+	}
+};
+
+Complex& operator++(Complex& op) {
+	op.real++;
+	op.img++;
+	return op;
+}
+
+Complex operator--(Complex& op, int x) {
+	Complex tmp = op;
+	op.real--;
+	op.img--;
+	return tmp;
+}
+
+
+
+
+int main() {
+	Complex x(2, -3);
+	++x;
+	cout << "증가결과 ";
+	x.show();
+	x--;
+	cout << "감소결과 ";
+	x.show();
+	return 0;
+}
+
+	
